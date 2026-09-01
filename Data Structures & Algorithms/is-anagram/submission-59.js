59fdfbfd91c5700575b923c7c046d815daa75367
Mira Mirani <49958457+MiraMirani@users.noreply.github.com>
2026-09-01 22:19:12 +0200

    Add: is-anagram - submission-59

---
+++ Data Structures & Algorithms/is-anagram/submission-59.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length!==t.length) return false;
+
+        let counts = new Map();
+        for(let i = 0; i < s.length ; i++) {
+            counts.set(s[i],(counts.get(s[i])?? 0)+1)
+            counts.set(t[i],(counts.get(t[i])?? 0)-1)
+        }
+        
+        for(let el of counts.values()) if(el !== 0) return false;
+        return true;
+
+    }
+}
