6b96117baf7c9ac35460f8c8060530ae8f830b76
Mira Mirani <49958457+MiraMirani@users.noreply.github.com>
2026-09-01 23:02:44 +0200

    Add: two-integer-sum - submission-30

---
+++ Data Structures & Algorithms/two-integer-sum/submission-30.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let temp_map = new Map()
+        for(let i = 0 ; i < nums.length; i++){
+            let j = temp_map.get(nums[i])
+            if( temp_map.has(nums[i]) &&  j !== i   ){
+                return [Math.min(i, j), Math.max(i,j)]
+            }
+            temp_map.set(target - nums[i], i) 
+
+        }
+        return []
+    }
+}
