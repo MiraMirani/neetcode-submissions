296e0e50d35cc4d3e0722aa89c5a7cc012d8af2e
Mira Mirani <49958457+MiraMirani@users.noreply.github.com>
2026-09-04 00:05:48 +0200

    Add: top-k-elements-in-list - submission-26

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-26.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+
+        let hash = new Map();
+
+        for(let el of nums){
+            hash.set(el, (hash.get(el)??0)+1 )
+ 
+        }
+        
+
+return [...hash.entries()].sort((a,b)=>b[1]-a[1]).slice(0,k).map(e=>e[0])
+    }
+}
