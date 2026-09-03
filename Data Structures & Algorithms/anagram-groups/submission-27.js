53402c2c764254075dbce8e3a598eab900d42d87
Mira Mirani <49958457+MiraMirani@users.noreply.github.com>
2026-09-03 09:38:24 +0200

    Add: anagram-groups - submission-27

---
+++ Data Structures & Algorithms/anagram-groups/submission-27.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+
+        let hashMap = new Map();
+    
+        for(let str of strs){
+            if(hashMap.has(this.sorter(str))){
+                console.log(111)
+                hashMap.get(this.sorter(str)).push(str)
+                
+
+            }else{
+                hashMap.set(this.sorter(str), [str])
+            }
+            
+        };
+
+        return [...hashMap.values()]
+    }
+
+    sorter (str){
+        return str.split("").sort().join("");
+    }
+
+    checkAnagram(s, t){
+        let hashMap = new Map();
+        if(t.length !== s.length ) return false
+        for(let i = 0 ; i < s.length ; i++){
+            hashMap.set(s[i], (hashMap.get(s[i])?? 0) + 1);
+            hashMap.set(t[i], (hashMap.get(t[i])?? 0) - 1);
+        }
+
+        for(let el of hashMap.values()){
+            if(el !== 0) return false;
+        }
+        return true;
+    } 
+}
